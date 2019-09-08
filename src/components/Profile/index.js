eb7ba1ebdff954809f9571ab29e977b65b909e0e
Fabrício Oliveira <fabloliv@outlook.com>
2019-09-08 04:11:29 -0300

    Create Profile Component

---
+++ src/components/Profile/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Profile = () => (
+    <div className="Profile-wrapper">
+        <h1>Matt Foo</h1>
+        <h2>Front-End Dev</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+    </div>
+)
+
+export default Profile
