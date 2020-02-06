8ab8d5cda791f373942e4d3aeefb412207f4bc40
Fabrício Oliveira <fabriciololiveira@gmail.com>
2020-02-06 03:35:41 -0300

    Add markdown data to Home

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,58 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
 import PostItem from "../components/PostItem"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <PostItem />
-  </Layout>
-)
+const IndexPage = () => {
+  const { allMarkdownRemark } = useStaticQuery(graphql`
+    query PostList {
+      allMarkdownRemark {
+        edges {
+          node {
+            timeToRead
+            wordCount {
+              words
+            }
+            frontmatter {
+              background
+              category
+              description
+              title
+              date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  const postList = allMarkdownRemark.edges
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      {postList.map(
+        ({
+          node: {
+            frontmatter: { background, category, date, description, title },
+            timeToRead,
+          },
+        }) => (
+          <PostItem
+            slug="/about/"
+            background={background}
+            category={category}
+            date={date}
+            timeToRead={timeToRead}
+            title={title}
+            description={description}
+          />
+        )
+      )}
+    </Layout>
+  )
+}
 
 export default IndexPage
